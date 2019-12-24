6e161c617fbd702a9de18c50d03a83195e8df6e3
rt342626926 <342626926@qq.com>
2019-12-24 16:17:00 +0800

    任涛：提交realTimeMonitors页面

---
+++ src/store/modules/realTimeMonitors.js
@@ -0,0 +1,20 @@
+import {get} from '../../http/axios'
+
+const state = {
+ 
+}
+
+const mutations = {
+ 
+}
+
+const actions = {
+  
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
--- src/views/monitors/realTimeMonitors.vue
+++ src/views/monitors/realTimeMonitors.vue
@@ -1,3 +1,166 @@
 <template>
-  <div class="realTime" />
+  <div class="realTime">
+    <div class="title">
+      <el-row :gutter="12">
+        <el-col :span="6">
+          <el-card shadow="hover">
+            实时用电量
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card shadow="hover">
+            实时用水量
+            
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card shadow="hover">
+            剩余额度
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card shadow="hover">
+            当月电费
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 折线图 -->
+    <div class="chart1">
+        <div id="myChart" :style="{width: '900px', height: '500px'}"></div>
+   
+        <div class="table">
+              <h3>设备能耗</h3>
+              <el-table
+                :data="tableData"
+                border
+                style="width: 100%">
+                <el-table-column
+                  prop="device"
+                  label="设备"
+                  width="150">
+                </el-table-column>
+                <el-table-column
+                  prop="i"
+                  label="电流"
+                  width="150">
+                </el-table-column>
+                <el-table-column
+                  prop="electricity"
+                  label="电压"
+                  width="150">
+                </el-table-column>
+                <el-table-column
+                  prop="water"
+                  label="用水"
+                  width="150">  
+                </el-table-column>
+              </el-table>
+        </div>
+       </div>
+  </div>
 </template>
+
+<script>
+export default {
+    name: 'test1',
+    data () {
+      return {
+        map: null
+      }
+    },
+    mounted () {
+      let that = this
+      this.drawLine();
+    },
+    methods:{
+      drawLine(){
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = this.$echarts.init(document.getElementById('myChart'))
+        // 绘制图表
+        var option = {
+              title: {
+        text: '实时信息'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['电表一','电表二','电表三','水表']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['8：00','8：30','9：00','9：30','10：00','10：30','11：00','11:30','12:00','12:30']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name:'电表一',
+            type:'line',
+            stack: '总量',
+            data:[120, 132, 101, 134, 90, 230, 210,230,140,110]
+        },
+        {
+            name:'电表二',
+            type:'line',
+            stack: '总量',
+            data:[220, 182, 191, 234, 290, 330, 310,150,162,213]
+        },
+        {
+            name:'电表三',
+            type:'line',
+            stack: '总量',
+            data:[150, 232, 201, 154, 190, 330, 410,169,198,206]
+        },
+        {
+            name:'水表',
+            type:'line',
+            stack: '总量',
+            data:[320, 332, 301, 334, 390, 330, 320,128,236,205]
+        }
+    ]
+        };
+
+        myChart.setOption(option)
+    }
+    }
+  }
+</script>
+
+<style scoped>
+.realTime{
+    background-color: rgba(255, 255, 255, 0.884);
+    border-radius:5px;
+    padding: 1em;
+}
+.chart1{
+  margin-top: 2em;
+ 
+}
+#myChart{
+ float: left;
+}
+.table{
+  float: left; 
+  size: "small"; 
+  margin-left: 2em;
+ 
+}
+.el-card{
+  background-color: rgba(63, 128, 193, 0.5);
+}
+</style>
