2dd1bde24ec3713d80e8bc0d8cff3c3b3914bab3
JAVCcxp <245047810@qq.com>
2019-12-26 15:51:08 +0800

    晁：页面

--- src/store/modules/historyMonitor.js
+++ src/store/modules/historyMonitor.js
@@ -2,11 +2,15 @@ import {get} from '../../http/axios'
 
 const state = {
   electricity:[],
+  water:[],
 }
 
 const mutations = {
   getElectricity(state,data){
     state.electricity = data;
+  },
+  getWater(state,data){
+    state.water = data;
   },
   
 }
@@ -16,9 +20,14 @@ const actions = {
     let response = await get('/selectEAll');
     commit('getElectricity',response.data)
   },
+
+  async findWater({commit},data){
+    let response = await get('/selectWAll');
+    console.log("aaaaaa")
+    commit('getWater',response.data)
+  },
  
 }
-
 export default {
   namespaced: true,
   state,
